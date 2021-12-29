7d8adbf7b49c8600d318758827504456d44ffafb
Alex Nguyen <46719079+alexnguyenn@users.noreply.github.com>
2021-12-28 22:28:40 -0800

    Change to 4 tab spaces for `App.js`

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import Posts from './components/Posts/Posts';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>What did I learned</h1>
-      <Posts />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>What did I learned</h1>
+            <Posts />
+        </div>
+    );
 }
 
 export default App;
