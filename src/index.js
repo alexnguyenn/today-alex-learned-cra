d11a686313dd041328d1c59d094e18c5586f4e15
Alex Nguyen <46719079+alexnguyenn@users.noreply.github.com>
2021-12-28 22:27:41 -0800

    Change to 4 tab spaces for `index.html`

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql
 } from "@apollo/client";
 import './index.css';
 
 const client = new ApolloClient({
-  uri: 'https://api-us-west-2.graphcms.com/v2/ckx5k6flg4kir01za8jhwcp89/master',
-  cache: new InMemoryCache()
+    uri: 'https://api-us-west-2.graphcms.com/v2/ckx5k6flg4kir01za8jhwcp89/master',
+    cache: new InMemoryCache()
 });
 
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
 );
